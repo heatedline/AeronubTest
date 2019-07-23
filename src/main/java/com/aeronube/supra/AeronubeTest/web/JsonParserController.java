1619318f6465993ed4a8a5c78e40447787b869f9
supratim <supratim.c@finlabsindia.com>
2019-07-23 08:13:52 +0530

    Latest commit

--- src/main/java/com/aeronube/supra/AeronubeTest/web/JsonParserController.java
+++ src/main/java/com/aeronube/supra/AeronubeTest/web/JsonParserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aeronube.supra.AeronubeTest.dto.BookDTO;
-import com.aeronube.supra.AeronubeTest.dto.SearchBookDTO;
 import com.aeronube.supra.AeronubeTest.service.JsonParserService;
 
 @RestController
